84d38e7dee731c263d4c6c9b135bf0a495c28e1f
Christopher Ross-Gill <chris.rg@ross-gill.com>
2017-02-06 11:27:53 -0500

    SCRIPT: HTTPd v0.2.0—Include HTTP Parser/Dispatcher.

--- httpd.r
+++ httpd.r
@@ -1,14 +1,15 @@
 Rebol [
 	Title: "Web Server Scheme for Rebol 2"
 	Author: "Christopher Ross-Gill"
-	Date: 12-Jan-2017
+	Date: 6-Feb-2017
 	File: %httpd.r
-	Version: 0.1.0
+	Version: 0.2.0
 	Purpose: "An elementary Web Server scheme for creating fast prototypes"
 	Rights: http://opensource.org/licenses/Apache-2.0
 	Type: 'module
 	Name: 'rgchris.httpd
 	History: [
+		06-Feb-2017 0.2.0 "Include HTTP Parser/Dispatcher"
 		12-Jan-2017 0.1.0 "Original Version"
 	]
 	Notes: {
@@ -24,89 +25,180 @@ unless in system/schemes 'httpd [
 set in system/schemes 'httpd make system/standard/port [
 	scheme: 'httpd
 	port-id: 8888
+
+	; Default Settings, Ignore
 	passive: #[none]
 	cache-size: 0
 	proxy: make object! [host: port-id: user: pass: type: bypass: #[none]]
+
+	; Handler
 	handler: make object! bind [
-		init: func [port [port!] spec][
-			use [digit port-number][
-				digit: charset "0123456789"
-				port-number: none
+		init: func [server [port!] spec][
+			if url? spec [
+				spec: decode-url spec
+			]
 
-				either all [
-					url? spec
-					parse/all spec [
-						"httpd://" opt [
-							":" copy port-number some digit end
-							(port-number: to integer! port-number)
-						]
-					]
-				][
-					port/sub-port: make port! [
-						scheme: 'tcp
-						port-id: any [port-number port/port-id]
-						async-modes: [connect]
-						locals: make binary! buffer-size: 64 * 1024
-					]
-				][
-					make error! "Invalid HTTPd Spec"
+			spec: make make object! [port-id: awake: none] spec
+			; probe spec
+
+			either integer? server/port-id: spec/port-id [
+				server/sub-port: make port! [
+					scheme: 'tcp
+					port-id: spec/port-id
+					async-modes: 'connect
 				]
+				server/state/flags: server/state/flags or system/standard/port-flags/pass-thru
+			][
+				make error! "Invalid HTTPd Spec"
 			]
 		]
 
-		open: func [port [port!]][
-			port/state/flags: port/state/flags or system/standard/port-flags/pass-thru
-			port/buffer-size: 64 * 1024
-
-			; port/awake: func [request [object!]] either block? port/state/custom [
-			; 	:port/state/custom
-			; ][
-			; 	[]
-			; ]
-
-			port/sub-port/awake: func [listen-port] use [handler][
-				handler: either block? port/state/custom [
-					port/state/custom
-				][
-					response: "I'm Alive!!!"
-				]
-
-				[
-					use [port part][
-						port: first listen-port
-						clear listen-port/locals
-						probe port/async-modes
-						; port/async-modes: [read write]
-					
-
-						; repend listen-port/locals ["Address: " port/host newline]
-						; set-modes port [no-wait: true]
-						read-io port listen-port/locals listen-port/buffer-size
-						probe transcribe port listen-port/locals
-						write-io port data: "HTTP/1.0 200 OK^/Content-type: text/plain^/^/I'm Alive!!!" length? data
-						system/words/close port
-					]
-					false
-				]
-			]
-
-			; set-modes port/sub-port [read: true write: true binary: false lines: true no-wait: false direct: true]
-			insert system/ports/wait-list port/sub-port
-			system/words/open/direct/binary port/sub-port
+		open: func [server [port!]][
+			server/sub-port/awake: prepare-listener server
+			net-utils/net-log rejoin ["Opening HTTP listening port " server/port-id]
+			insert system/ports/wait-list server/sub-port
+			system/words/open/direct/binary server/sub-port
 		]
 
-		close: func [port [port!]][
-			probe "CLOSED!!!"
-			remove find system/ports/wait-list port/sub-port
-			system/words/close port/sub-port
+		close: func [server [port!]][
+			net-utils/net-log rejoin ["Closing HTTP listening port " server/port-id]
+			remove find system/ports/wait-list server/sub-port
+			system/words/close server/sub-port
 		]
+
 	] make object! [
-		transcribe: use [
-			request-uri version-number
-			header-feed header-name header-part
-			request-prototype header-prototype
+		buffer: make binary! 2 + buffer-size: 64 * 1024
+
+		request-prototype: make object! [
+			version: 1.1
+			method: "GET"
+			action: headers: http-headers: none
+			oauth: target: binary: content: length: timeout: none
+			type: 'application/x-www-form-urlencoded
+			server-software: rejoin [
+				system/script/header/title " v" system/script/header/version " "
+				"Rebol/" system/product " v" system/version
+			]
+			server-name: gateway-interface: none
+			server-protocol: "http"
+			server-port: request-method: request-uri:
+			path-info: path-translated: script-name: query-string:
+			remote-host: remote-addr: auth-type:
+			remote-user: remote-ident: content-type: content-length: none
+			error: none
+		]
+
+		response-prototype: make object! [
+			status: 404
+			content: "Not Found"
+			location: none
+			type: "text/html"
+			kill?: false
+		]
+
+		start: func [port [port!]][
+			append system/ports/wait-list port
+		]
+
+		stop: func [port [port!]][
+			remove find system/ports/wait-list port
+			close port
+		]
+
+		num: 0
+
+		wake-client: func [client [port!] /local state wire request response this][
+			this: num
+			++ num
+
+			while [
+				case [
+					error? state: try [
+						read-io client clear buffer buffer-size
+					][
+						state: disarm :state
+						either state/code = 517 [ ; would-block
+							; end of input?
+							net-utils/net-log rejoin ["[" this "] End of Stream"]
+						][
+							net-utils/net-log rejoin ["[" this "] Read Error"]
+							help state
+						]
+						false
+					]
+
+					state <= 0 [
+						net-utils/net-log rejoin ["[" this "] Client closed connection: " state]
+						; probe buffer
+						false
+					]
+
+					state [true]
+				]
+			][
+				net-utils/net-log rejoin ["[" this "] Reading stream..."]
+				append client/locals/wire buffer
+			]
+
+			transcribe client
+			dispatch client
+
+			net-utils/net-log rejoin ["[" this "] Responding..."]
+			client/async-modes: 'write
+			write-io client as-string client/locals/wire length? client/locals/wire
+			stop client
+
+			client/locals/response/kill?
+		]
+
+		prepare-listener: func [
+			server [port!] /local handler
 		][
-			request-uri: use [chars][
+			handler: func [
+				request [object!]
+				response [object!]
+			] case [
+				function? get in server 'awake [body-of get in server 'awake]
+				block? server/awake [server/awake]
+				block? server/state/custom [server/state/custom]
+				true [default-response]
+			]
+
+			; listener AWAKE function
+			func [listener [port!] /local client part state] compose [
+				; help listener
+				client: first listener
+				; help client
+
+				net-utils/net-log reform ["HTTP request from" client/host]
+				; probe client/user-data
+
+				client/async-modes: 'read
+				client/awake: :wake-client
+				client/local-service: quote (:handler)
+				client/locals: make object! [
+					request: response: none
+					wire: make binary! 0
+					protect [request response wire]
+				]
+
+				start client
+				false
+			]
+		]
+
+		transcribe: use [
+			request-action request-path request-query
+			header-prototype header-feed header-name header-part
+		][
+			request-action: ["HEAD" | "GET" | "POST" | "PUT" | "DELETE"]
+
+			request-path: use [chars][
+				chars: complement charset [#"^@" - #" " #"?"]
+				[some chars]
+			]
+
+			request-query: use [chars][
 				chars: complement charset [#"^@" - #" "]
 				[some chars]
 			]
@@ -128,15 +220,6 @@ set in system/schemes 'httpd make system/standard/port [
 				[some space]
 			]
 
-			request-prototype: context [
-				version: 1.1
-				action: "GET"
-				headers: http-headers: none
-				query: none
-				oauth: target: binary: content: length: timeout: none
-				type: 'application/x-www-form-urlencoded
-			]
-
 			header-prototype: context [
 				Accept: "*/*"
 				Connection: "close"
@@ -144,38 +227,94 @@ set in system/schemes 'httpd make system/standard/port [
 				Content-Length: Content-Type: Authorization: Range: none
 			]
 
-			transcribe: func [port [port!] request-data [binary!] /local request name value pos][
-				port/locals: request: make request-prototype [
-					unless parse/all request-data [
-						copy action ["HEAD" | "GET" | "POST" | "PUT" | "DELETE"] space
-						copy target request-uri space
+			transcribe: func [
+				client [port!]
+				/local request name value pos
+			][
+				client/locals/request: make request-prototype [
+					either parse/all client/locals/wire [
+						copy method request-action space
+						copy request-uri [
+							copy target request-path opt [
+								"?" copy query-string request-query
+							]
+						] space
 						"HTTP/" copy version ["1.0" | "1.1"]
 						header-feed
-						(net-utils/net-log reform ["HTTP Request:" action target])
-						(
-							headers: make block! []
-						)
+						(headers: make block! [])
 						some [
 							copy name header-name ":" any " "
 							copy value header-part header-feed
-							(repend headers [to set-word! name value])
+							(
+								repend headers [to set-word! name value]
+								switch name [
+									"Content-Type" [content-type: value]
+									"Content-Length" [length: content-length: value]
+								]
+							)
 						]
 						header-feed content: to end (
 							content: as-string binary: as-binary copy content
 						)
 					][
-						make error! "Could Not Parse Request"
+						request-method: :method
+						path-info: :target
+						action: reform [method target]
+						remote-addr: client/remote-ip
+
+						headers: make header-prototype http-headers: new-line/all/skip headers true 2
+
+						type: if string? headers/Content-Type [
+							copy/part type: headers/Content-Type any [
+								find type ";"
+								tail type
+							]
+						]
+
+						length: content-length: any [
+							attempt [length: to integer! length]
+							0
+						]
+
+						net-utils/net-log action
+					][
+						; action: target: request-method: query-string: binary: content: request-uri: none
+						net-utils/net-log error: "Could Not Parse Request"
 					]
-
-					headers: make header-prototype http-headers: new-line/all/skip headers true 2
-
-					type: all [
-						path? type: attempt [load headers/Content-Type]
-						type
-					]
-
-					length: any [attempt [headers/Content-Length: to integer! headers/Content-Length] 0]
 				]
+			]
+		]
+
+		dispatch: use [status-codes][
+			status-codes: [
+				200 "OK" 201 "Created" 204 "No Content"
+				301 "Moved Permanently" 302 "Moved temporarily" 303 "See Other" 307 "Temporary Redirect"
+				400 "Bad Request" 401 "No Authorization" 403 "Forbidden" 404 "Not Found" 411 "Length Required"
+				500 "Internal Server Error" 503 "Service Unavailable"
+			]
+
+			func [client [port!] /local response continue?][
+				client/locals/response: response: make response-prototype []
+				client/local-service client/locals/request response
+				head collect/into [
+					case/all [
+						not find status-codes response/status [
+							response/status: 500
+						]
+						any [not any-string? response/content empty? response/content][
+							response/content: " "
+						]
+					]
+
+					keep reform ["HTTP/1.0" response/status select status-codes response/status]
+					keep reform ["^/Content-Type:" response/type]
+					keep reform ["^/Content-Length:" length? response/content]
+					if response/location [
+						keep reform ["^/Location:" response/location]
+					]
+					keep "^/^/"
+					keep response/content
+				] clear client/locals/wire
 			]
 		]
 	]
